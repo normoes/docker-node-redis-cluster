2cc90d7c30c7233b582eea40f2111520bfa339ae
normoes <normo@posteo.de>
2017-03-08 14:25:45 +0100

    added script that builds containers

---
+++ build_containers.sh
@@ -0,0 +1,16 @@
+cd node
+docker build -t normoes/node . --no-cache
+cd ..
+
+cd redis_protectedModeOff
+cd redis_base
+docker build -t normoes/redis . --no-cache
+cd redis_primary
+docker build -t normoes/redis_primary . --no-cache
+cd redis_replica
+docker build -t normoes/redis_replica . --no-cache
+cd ../..
+
+cd logstash
+docker build -t normoes/logstash . --no-cache
+cd ..
