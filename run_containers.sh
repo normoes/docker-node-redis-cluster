f746ca40b8babe92ff900054979602ff1b5bf6f0
normoes <normo@posteo.de>
2017-03-08 14:10:43 +0100

    initial commit

---
+++ run_containers.sh
@@ -0,0 +1,23 @@
+echo $1
+
+
+express_net=$(docker network ls --filter name=^express$ -q)
+echo $express_net
+if [ "$express_net" == "" ];then
+  echo "---->"
+  echo "    creating docker nework express..."
+  echo "---->"
+  docker network create express
+fi
+unset express_net 
+
+
+
+docker run -d -p 3000:3000 --net express -h nodeapp --name nodeapp normoes/node
+
+docker run -d -p 6379:6379 --net express -h redis-primary --name redis_primary normoes/redis_primary
+
+docker run -d --net express -h redis-replica1 --name redis_replica1 normoes/redis_replica
+
+docker run -d --net express --name logstash --volumes-from redis_primary --volumes-from nodeapp normoes/logstash
+
---
+++ stop_containers.sh
@@ -0,0 +1,11 @@
+docker stop nodeapp
+docker rm nodeapp
+
+docker stop redis_primary
+docker rm redis_primary
+
+docker stop redis_replica1
+docker rm redis_replica1
+
+docker stop logstash
+docker rm logstash
