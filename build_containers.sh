74243f5504b2a64808fb21c4c1560caef0d35bdc
normo@posteo.de <normo@posteo.de>
2017-03-09 08:19:32 -0800

    cd .. added

--- build_containers.sh
+++ build_containers.sh
@@ -5,8 +5,10 @@ cd ..
 cd redis_protectedModeOff
 cd redis_base
 docker build -t normoes/redis . --no-cache
+cd ..
 cd redis_primary
 docker build -t normoes/redis_primary . --no-cache
+cd ..
 cd redis_replica
 docker build -t normoes/redis_replica . --no-cache
 cd ../..
